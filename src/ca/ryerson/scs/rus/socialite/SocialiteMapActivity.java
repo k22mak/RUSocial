2a25f610820a9be291e10cfd6c5ed059f21ac86c
Kelvin Mak <kelvin.mak@ryerson.ca>
2013-03-02 14:44:32 -0500

    Implemented Google Maps API into socialize map activity.
    Fixed #5

--- src/ca/ryerson/scs/rus/socialite/SocialiteMapActivity.java
+++ src/ca/ryerson/scs/rus/socialite/SocialiteMapActivity.java
@@ -1,62 +1,102 @@
 package ca.ryerson.scs.rus.socialite;
 
-import ca.ryerson.scs.rus.MenuActivity;
-import ca.ryerson.scs.rus.R;
-import ca.ryerson.scs.rus.SplashActivity;
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class SocialiteMapActivity extends Activity implements OnClickListener {
-	
-	private Button btnListView, btnProfile, btnBack;
-	private final String SOCIALITE_LIST_STRING="ca.ryerson.scs.rus.socialite.SOCIALITE_LIST";
-	private final String PROFILE_STRING = "ca.ryerson.scs.rus.messenger.PROFILE";
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.socialize_map);
-		
-		btnListView = (Button) findViewById(R.id.BtnSocialiteListView);
-		btnProfile = (Button) findViewById(R.id.BtnProfile);
-		btnBack = (Button) findViewById(R.id.BtnBack);
-		
-		
-		btnListView.setFocusable(true);
-		btnProfile.setFocusable(true);
-		btnBack.setFocusable(true);
-		
-		btnListView.setOnClickListener(this);
-		btnProfile.setOnClickListener(this);
-		btnBack.setOnClickListener(this);
-		
-	}
-	
-	@Override
-	public void onClick(View v) {
-		if (v == btnBack) {
-			if (SplashActivity.DEBUG) {
-				Log.i(MenuActivity.TAG, "Back Button");
-			}
-			// TODO: Make it go back to the main page while finishing all other activities			
-		} else if (v == btnProfile) {
-			if (SplashActivity.DEBUG) {
-				Log.i(MenuActivity.TAG, "Profile Button");
-			}
-			startActivity(new Intent(PROFILE_STRING));
-		}else if (v == btnListView) {
-			if (SplashActivity.DEBUG) {
-				Log.i(MenuActivity.TAG, "List View Button");
-			}
-			finish();
-			startActivity(new Intent(SOCIALITE_LIST_STRING));
-		}
-	}
+import android.view.Menu;
 
+import ca.ryerson.scs.rus.R;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
+
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class SocialiteMapActivity extends Activity implements LocationListener {
+  static final LatLng HAMBURG = new LatLng(53.558, 9.927);
+  static final LatLng KIEL = new LatLng(53.551, 9.993);
+  private GoogleMap map;
+  LocationManager locationManager;
+  private static final long MIN_TIME = 400;
+  private static final float MIN_DISTANCE = 1000;
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.socialize_map);
+	    GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+	      
+
+	    
+	    map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+	        .getMap();
+	    locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);         
+
+
+	    /*
+	    Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
+	        .title("Hamburg"));
+	    Marker kiel = map.addMarker(new MarkerOptions()
+	        .position(KIEL)
+	        .title("Kiel")
+	        .snippet("Kiel is cool")
+	        .icon(BitmapDescriptorFactory
+	            .fromResource(R.drawable.ic_launcher)));
+*/
+	    // Move the camera instantly to hamburg with a zoom of 15.
+	    //map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
+
+	    // Zoom in, animating the camera.
+	    //map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    //getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
+  
+  @Override
+  public void onLocationChanged(Location location) {
+      LatLng defaultLocation = new LatLng(location.getLatitude(), location.getLongitude());
+      CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(defaultLocation, 17);
+      map.moveCamera(cameraUpdate);
+      map.addMarker(new MarkerOptions().position(defaultLocation));
+      locationManager.removeUpdates(this);
+  }
+
+
+@Override
+public void onProviderDisabled(String provider) {
+	// TODO Auto-generated method stub
+	
 }
+
+
+@Override
+public void onProviderEnabled(String provider) {
+	// TODO Auto-generated method stub
+	
+}
+
+
+@Override
+public void onStatusChanged(String provider, int status, Bundle extras) {
+	// TODO Auto-generated method stub
+	
+}
+
+} 
