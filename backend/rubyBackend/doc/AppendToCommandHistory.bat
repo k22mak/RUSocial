5c3074fed9ed11c848452b00a9e973f3450dffe3
Christopher Chan <ssbattousai@gmail.com>
2013-03-02 17:59:51 -0500

    Removed CommandHistory and the bat file to auto update.

--- backend/rubyBackend/doc/AppendToCommandHistory.bat
+++
@@ -1,7 +0,0 @@
-REM http://www.codeproject.com/Tips/119828/Running-a-bat-file-as-administrator-Correcting-cur
-@setlocal enableextensions
-@cd /d "%~dp0"
-set OLDDIR=%CD%
-echo "Command History of the date" >> "CommandHistory.txt"
-date /t  >> "CommandHistory.txt"
-DOSKEY /history >> "CommandHistory.txt"
