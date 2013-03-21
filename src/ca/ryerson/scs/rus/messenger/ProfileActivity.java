7e8685b9710dde7fb8ac8c2a27dd0fcbfb4097ac
T. Perez <trperez221@gmail.com>
2013-03-20 21:13:19 -0400

    Added menu bars
    Issue #10

--- src/ca/ryerson/scs/rus/messenger/ProfileActivity.java
+++ src/ca/ryerson/scs/rus/messenger/ProfileActivity.java
@@ -1,24 +1,87 @@
 package ca.ryerson.scs.rus.messenger;
 
+import ca.ryerson.scs.rus.MenuActivity;
 import ca.ryerson.scs.rus.R;
+import ca.ryerson.scs.rus.SplashActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class ProfileActivity extends Activity implements OnClickListener {
+	private ImageButton btnMapView, btnHome, btnMsg, btnPref, btnFriend;
+	private final String SOCIALITE_MAP_STRING="ca.ryerson.scs.rus.socialite.SOCIALITE_MAP";
+	private final String MESSAGE_STRING="ca.ryerson.scs.rus.messenger.MESSAGES_LIST";
+	private final String PREFERENCE_STRING="ca.ryerson.scs.rus.PREFERENCES";
+	private final String FRIEND_STRING="ca.ryerson.scs.rus.messenger.FRIENDS_LIST";
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.profile);
+		
+		btnMapView = (ImageButton) findViewById(R.id.IBLook);
+		btnHome = (ImageButton) findViewById(R.id.IBHome);
+		btnMsg = (ImageButton) findViewById(R.id.IBMsg);
+		btnPref = (ImageButton) findViewById(R.id.IBPref);
+		btnFriend = (ImageButton) findViewById(R.id.IBFriend);
+		
+				
+		btnMapView.setFocusable(true);
+		btnHome.setFocusable(true);
+		btnMsg.setFocusable(true);
+		btnPref.setFocusable(true);
+		btnFriend.setFocusable(true);
+		
+		btnMapView.setOnClickListener(this);
+		btnHome.setOnClickListener(this);
+		btnMsg.setOnClickListener(this);
+		btnPref.setOnClickListener(this);
+		btnFriend.setOnClickListener(this);
+	
 	}
 	
 	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
+	public void onClick(View v) {
+		if (v == btnHome) {
+			if (SplashActivity.DEBUG) {
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Home button");
+			}
+			// TODO: Make it go back to the main page while finishing all other activities
+			
+		}else if (v == btnMapView) {
+			if (SplashActivity.DEBUG) {
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Map View Button");
+			}
+			finish();
+			startActivity(new Intent(SOCIALITE_MAP_STRING));
 		
-	}
+		}else if (v == btnMsg) {
+			if (SplashActivity.DEBUG){
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Message Button");
+			}
+			finish();
+			startActivity(new Intent(MESSAGE_STRING));
+		
+		}else if (v == btnFriend) {
+			if (SplashActivity.DEBUG){
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Friend Button");
+			}
+			finish();
+			startActivity(new Intent(FRIEND_STRING));
+			
+		}else if (v == btnPref) {
+			if (SplashActivity.DEBUG){
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Preference Button");
+				}
+			finish();
+			startActivity(new Intent(PREFERENCE_STRING));
+			}
+		}
 
 }
