58c657c3ade7f27f88e4f5b752601b58cde492e4
Kelvin Mak <kelvin.mak@ryerson.ca>
2013-03-27 16:21:50 -0400

    Made HTTPRequest Adapter to be async
    Fixed #26

--- src/ca/ryerson/scs/rus/LoginActivity.java
+++ src/ca/ryerson/scs/rus/LoginActivity.java
@@ -1,7 +1,13 @@
 package ca.ryerson.scs.rus;
 
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ca.ryerson.scs.rus.util.HttpRequestAdapter;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +15,7 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity implements OnClickListener {
 	// Flag for debug/log messages
@@ -17,11 +24,12 @@ public class LoginActivity extends Activity implements OnClickListener {
 	// Tag for debugging
 	public static final String TAG = "RyeSocial";
 
+	private Context context;
+	
 	private final String MENU_STRING = "ca.ryerson.scs.rus.MENU";
 	private final String REGISTER_STRING = "ca.ryerson.scs.rus.REGISTER";
 
 	private TextView btnSubmit, btnRegister;
-
 	private EditText evUsername, evPassword;
 
 	@Override
@@ -29,6 +37,8 @@ public class LoginActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.login);
+		
+		context=this;
 
 		btnSubmit = (TextView) findViewById(R.id.BtnSubmit);
 		btnRegister = (TextView) findViewById(R.id.BtnRegister);
@@ -53,7 +63,16 @@ public class LoginActivity extends Activity implements OnClickListener {
 				Log.i(TAG, evUsername.getText() + " ");
 				Log.i(TAG, evPassword.getText() + " ");
 			}
-
+			JSONObject json = new JSONObject();
+			try {
+				json.put("username", evUsername.getText().toString());
+				json.put("password", evPassword.getText().toString());
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			HttpRequestAdapter.httpRequest(this, "http://24.52.208.137:3000/logins",json,new LoginHandler());
+			
 			// TODO: Create URL using inputs and make an HTTTP Request for
 			// authentication
 			startActivity(new Intent(MENU_STRING));
@@ -62,4 +81,17 @@ public class LoginActivity extends Activity implements OnClickListener {
 		}
 
 	}
+	
+	private class LoginHandler implements HttpRequestAdapter.ResponseHandler {
+		@Override
+		public void postResponse(JSONObject response) {
+			//TODO: Parse the response and act accordingly here
+		}
+
+		@Override
+		public void postTimeout() {
+			Toast.makeText(context, "Connection timed out", Toast.LENGTH_LONG).show();
+		}
+	}
+
 }
--- src/ca/ryerson/scs/rus/util/HttpRequestAdapter.java
+++ src/ca/ryerson/scs/rus/util/HttpRequestAdapter.java
@@ -3,38 +3,43 @@ package ca.ryerson.scs.rus.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
 public class HttpRequestAdapter {
 
-	public static JSONObject httpRequest(String url,
-			HashMap<String, String> inputs) {
-
-		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-		for (Map.Entry<String, String> entry : inputs.entrySet()) {
-			nameValuePairs.add(new BasicNameValuePair(entry.getKey(), entry
-					.getValue()));
-		}
-
+	
+	public static void httpRequest(Context context, String url, ResponseHandler handler){
+		HttpRequestAdapter.Request request= new HttpRequestAdapter.Request(context,handler);
+		request.execute(url);
+	}
+	public static void httpRequest(Context context, String url, JSONObject data, ResponseHandler handler){
+		HttpRequestAdapter.Request request= new HttpRequestAdapter.Request(context,data,handler);
+		request.execute(url);
+	}
+	
+	private static JSONObject httpProcessRequest(Context context, String url,
+			JSONObject data) {
 		try {
 
 			DefaultHttpClient httpClient = new DefaultHttpClient();
 			HttpPost httpPost = new HttpPost(url);
-			httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			
+	        StringEntity se = new StringEntity(data.toString());
+	        httpPost.setEntity(se);
+	        httpPost.setHeader("Content-Type","application/json");
+	        
 			HttpResponse httpResponse = httpClient.execute(httpPost);
 
 			HttpEntity httpEntity = httpResponse.getEntity();
@@ -65,7 +70,7 @@ public class HttpRequestAdapter {
 		return null;
 	}
 
-	public static JSONObject httpRequest(String url) {
+	private static JSONObject httpProcessRequest(Context context, String url) {
 
 		try {
 
@@ -99,5 +104,53 @@ public class HttpRequestAdapter {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	private static class Request extends AsyncTask<String, Integer, JSONObject> {
+
+		private ProgressDialog dialog;
+		private Context context;
+		private JSONObject data;
+		private ResponseHandler handler;
+		public Request(Context context,ResponseHandler handler) {
+			this.context = context;
+			this.handler=handler;
+		}
+		
+		public Request(Context context,JSONObject data,ResponseHandler handler) {
+			this.context = context;
+			
+			this.handler=handler;
+		}
+		
+		@Override
+		protected void onPreExecute() {
+			dialog.setMessage("Connecting to Server");
+			dialog.show();
+		}
+		
+		@Override
+		protected JSONObject doInBackground(String... url) {
+			JSONObject response = null;
+			if (data==null){
+				response=HttpRequestAdapter.httpProcessRequest(context, url[0]);
+			}else if(data!=null){
+				response=HttpRequestAdapter.httpProcessRequest(context, url[0],data);
+			}			
+			return response;
+		}
+		
+		@Override
+		protected void onPostExecute(JSONObject result) {
+			handler.postResponse(result);
+		}
+	}
+	
+	public interface ResponseHandler {
+		
+		public void postResponse(JSONObject response);
+
+		public void postTimeout();
 	}
 }
+
