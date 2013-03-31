9b0c7af61a97f3c357e8d4a364fb8b458e723da0
T. Perez <trperez221@gmail.com>
2013-03-31 00:51:06 -0400

    New msg activity

--- src/ca/ryerson/scs/rus/messenger/NewMessageActivity.java
+++ src/ca/ryerson/scs/rus/messenger/NewMessageActivity.java
@@ -1,5 +1,117 @@
 package ca.ryerson.scs.rus.messenger;
 
-public class NewMessageActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+import ca.ryerson.scs.rus.MenuActivity;
+import ca.ryerson.scs.rus.R;
+import ca.ryerson.scs.rus.SplashActivity;
+import ca.ryerson.scs.rus.adapters.HttpRequestAdapter;
+import ca.ryerson.scs.rus.adapters.MessageListAdapter;
+import ca.ryerson.scs.rus.messenger.objects.Message;
+import ca.ryerson.scs.rus.util.DefaultUser;
+import ca.ryerson.scs.rus.util.IntentRes;
+import ca.ryerson.scs.rus.util.ProcessList;
+import ca.ryerson.scs.rus.util.URLResource;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.app.Activity;
+import android.view.View.OnClickListener;
 
+public class NewMessageActivity extends Activity implements OnClickListener{
+	private ImageButton btnMapView, btnHome, btnMsg, btnPref, btnFriend;
+	
+	//MessageListAdapter mla;
+	Context context;
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.new_message);
+
+		context = this;
+		
+		btnMapView = (ImageButton) findViewById(R.id.IBLook);
+		btnHome = (ImageButton) findViewById(R.id.IBHome);
+		btnMsg = (ImageButton) findViewById(R.id.IBMsg);
+		btnPref = (ImageButton) findViewById(R.id.IBPref);
+		btnFriend = (ImageButton) findViewById(R.id.IBFriend);
+
+		btnMapView.setFocusable(true);
+		btnHome.setFocusable(true);
+		btnMsg.setFocusable(true);
+		btnPref.setFocusable(true);
+		btnFriend.setFocusable(true);
+
+		btnMapView.setOnClickListener(this);
+		btnHome.setOnClickListener(this);
+		btnMsg.setOnClickListener(this);
+		btnPref.setOnClickListener(this);
+		btnFriend.setOnClickListener(this);
+	} 
+	
+	@Override
+	public void onClick(View v){
+		if (v == btnHome) {
+			if (SplashActivity.DEBUG) {
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Home button");
+			}
+			// TODO: Make it go back to the main page while finishing all other activities
+			
+		}else if (v == btnMapView) {
+			if (SplashActivity.DEBUG) {
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Map View Button");
+			}
+			Intent newIntent = new Intent(IntentRes.SOCIALITE_MAP_STRING);
+			newIntent.putExtra("username", DefaultUser.getUser());
+			finish();
+			startActivity(newIntent);
+		
+		}else if (v == btnMsg) {
+			if (SplashActivity.DEBUG){
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Message Button");
+			}
+			Intent newIntent = new Intent(IntentRes.MESSAGE_STRING);
+			newIntent.putExtra("username", DefaultUser.getUser());
+			finish();
+			startActivity(newIntent);
+		
+		}else if (v == btnFriend) {
+			if (SplashActivity.DEBUG){
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Friend Button");
+			}
+			Intent newIntent = new Intent(IntentRes.FRIEND_STRING);
+			newIntent.putExtra("username", DefaultUser.getUser());
+			finish();
+			startActivity(newIntent);
+			
+			
+		}else if (v == btnPref) {
+			if (SplashActivity.DEBUG){
+				if (SplashActivity.DEBUG)Log.i(MenuActivity.TAG, "Preference Button");
+				}
+			Intent newIntent = new Intent(IntentRes.PREFERENCE_STRING);
+			newIntent.putExtra("username", DefaultUser.getUser());
+			finish();
+			startActivity(newIntent);
+			}
+		
+	}
+	
+	/*@Override
+	public void postTimeout() {
+		// TODO Auto-generated method stub		
+	}
+	*/
 }
+
+
